4b514665502a6957f224be14a7bbee67357bb6f2
Tomek Muszynski <32245713+muchasty@users.noreply.github.com>
2018-12-05 00:23:47 +0100

    version 1.21
    Update to Blender 2.80

---
+++ autoUVlm_2-80.py
@@ -0,0 +1,129 @@
+bl_info = {
+    "name": "Lightmap Auto UV",
+    "description": "Script makes automatic UV unwrap for lightmaps and helps to avoid a pixel sharing issue by islands.",
+    "author": "Tomasz Muszynski",
+    "blender":(2,80,0),
+    "version": (1, 21),
+    "tracker_url": "",
+    "support": "COMMUNITY",
+    "category": "UV",
+    "location": "View3D > Menu > Object > Lightmap Auto UV",
+    "tracker_url": "https://github.com/muchasty/Lightmap-Auto-UV",
+    }
+
+    
+import bpy
+from bpy.props import *
+
+class LightmapAutoUV(bpy.types.Operator):
+    """Lightmap Auto UV"""      # blender will use this as a tooltip for menu items and buttons.
+    bl_idname = "object.auto_lightmap"        # unique identifier for buttons and menu items to reference.
+    bl_label = "Lightmap Auto UV"    # display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+    
+    
+    lightmap_Resolution=IntProperty(name="Lightmap Resolution [px]", min=4, max=65536)
+    lightmap_KeepEditMode=BoolProperty(name="Keep Edit Mode")
+    lightmap_Overwrite=BoolProperty(name="Auto-LM Overwrite")
+    lightmap_Aspect=BoolProperty(name="Keep aspect ratio")
+    lightmap_Bounds=BoolProperty(name="Stretch (no-aspect)")
+    lightmap_Angle=FloatProperty(name="Angle limit", min=1, max=89)
+
+    
+    def execute(self, context):
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')         
+        try:    
+            if self.lightmap_Overwrite==True :           
+                bpy.context.active_object.data.uv_layers.remove(bpy.context.active_object.data.uv_layers["SmartUV Lightmap"])
+        except:
+            pass
+        
+        bpy.context.active_object.data.uv_layers.new(name="SmartUV Lightmap")
+        bpy.context.active_object.data.uv_layers["SmartUV Lightmap"].active=True
+
+        computedMargin=4*(1/self.lightmap_Resolution)
+        print(self.lightmap_Resolution ," = ",computedMargin)
+        bpy.ops.uv.smart_project(angle_limit=self.lightmap_Angle,island_margin=computedMargin, user_area_weight=1.0, use_aspect=self.lightmap_Aspect, stretch_to_bounds=self.lightmap_Bounds)
+        
+        bpy.context.scene["lightmap_Resolution"]=self.lightmap_Resolution
+        bpy.context.scene["lightmap_KeepEditMode"]=self.lightmap_KeepEditMode
+        bpy.context.scene["lightmap_Overwrite"]=self.lightmap_Overwrite
+        bpy.context.scene["lightmap_Aspect"]=self.lightmap_Aspect 
+        bpy.context.scene["lightmap_Bounds"]=self.lightmap_Bounds
+        bpy.context.scene["lightmap_Angle"]=self.lightmap_Angle
+    
+        if self.lightmap_KeepEditMode==False :    
+            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.context.active_object.data.uv_layers[0].active=True
+        
+    
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):                            
+        try:        
+            self.lightmap_Resolution=bpy.context.scene["lightmap_Resolution"]
+            self.lightmap_KeepEditMode=bpy.context.scene["lightmap_KeepEditMode"]
+            self.lightmap_Overwrite=bpy.context.scene["lightmap_Overwrite"]
+            self.lightmap_Aspect=bpy.context.scene["lightmap_Aspect"]
+            self.lightmap_Bounds=bpy.context.scene["lightmap_Bounds"]
+            self.lightmap_Angle=bpy.context.scene["lightmap_Angle"]
+        except:
+            bpy.context.scene["lightmap_Resolution"]=256    
+            bpy.context.scene["lightmap_KeepEditMode"]=False
+            bpy.context.scene["lightmap_Overwrite"]=True
+            bpy.context.scene["lightmap_Aspect"]=True 
+            bpy.context.scene["lightmap_Bounds"]=True
+            bpy.context.scene["lightmap_Angle"]=89
+        
+        
+        self.lightmap_Resolution=bpy.context.scene["lightmap_Resolution"]
+        self.lightmap_KeepEditMode=bpy.context.scene["lightmap_KeepEditMode"]
+        self.lightmap_Overwrite=bpy.context.scene["lightmap_Overwrite"]
+        self.lightmap_Aspect=bpy.context.scene["lightmap_Aspect"]
+        self.lightmap_Bounds=bpy.context.scene["lightmap_Bounds"]
+        self.lightmap_Angle=bpy.context.scene["lightmap_Angle"]
+        
+        return context.window_manager.invoke_props_dialog(self)
+    
+
+
+
+    
+    
+
+class LightmapAutoUVPanel(bpy.types.Panel):
+    bl_label = "Dialog"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    global lightmap_Resolution
+    def draw(self, context):
+        self.layout.operator("object.auto_lightmap")
+
+
+        
+def menu_func(self, context):
+    self.layout.operator(LightmapAutoUV.bl_idname)
+
+
+def register():
+    bpy.utils.register_class(LightmapAutoUV)
+    bpy.utils.register_class(LightmapAutoUVPanel)    
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+def unregister():
+    bpy.utils.unregister_class(LightmapAutoUV)
+    bpy.utils.unregister_class(LightmapAutoUVPanel)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
+
+    
+if __name__ == "__main__":
+    register()
+
+#--------------------------------- RUN ---------------------
+#bpy.ops.object.auto_lightmap('INVOKE_DEFAULT')
+
+
+
+
+
